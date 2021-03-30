4695051e17a16993bccb7ca9df8591da8c182613
agavrel <gavrel.antonin@gmail.com>
2021-03-30 14:26:59 -0400

    [ADD] Program logic

--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -3,66 +3,63 @@ use tokio::{sync::mpsc, task, time}; // 1.3.0
 
 #[tokio::main]
 async fn main() {
+    // mpsc::channel(3000)::<(usize, SocketAddr)>(3300);
 
-        // mpsc::channel(3000)::<(usize, SocketAddr)>(3300);
+    let mut start = false;
 
- let mut start = false;
-
-let (debounce_tx, mut debounce_rx) = mpsc::channel::<Vec<u8>>(3300); // mpsc::channel<Vec<u8>>(3300);
+    let (debounce_tx, mut debounce_rx) = mpsc::channel::<Vec<u8>>(3300); // mpsc::channel<Vec<u8>>(3300);
     let (network_tx, mut network_rx) = mpsc::channel::<Vec<u8>>(3300);
     // Listen for events
     let debouncer = task::spawn(async move {
         let duration = Duration::from_millis(3000);
-        let mut total_list_of_ids:Vec<i32> = Vec::new();
+        let mut total_list_of_ids: Vec<i32> = Vec::new();
         total_list_of_ids = vec![0; 6];
 
         loop {
             match time::timeout(duration, debounce_rx.recv()).await {
                 Ok(Some(bytes)) => {
-                eprintln!("Network activity");
+                    // TODO: add mutex for very first packet
 
-                        let id = bytes.clone()[0];
-                        eprintln!("id: {}", id);
-                         total_list_of_ids[id as usize] = 1;
-                         eprintln!("{:?}", total_list_of_ids);
-
+                    eprintln!("Network activity");
+                    let id = bytes.clone()[0];
+                    eprintln!("id: {}", id);
+                    total_list_of_ids[id as usize] = 1;
+                    eprintln!("{:?}", total_list_of_ids);
                 }
                 Ok(None) => {
-                    // Send the missing data back to the other side
-
-                    if start == true {
+                    if total_list_of_ids.iter().all(|x| x == &1i32) {
+                        // write file
                         eprintln!("Debounce finished");
+                        start = false;
                         break;
                     }
                 }
                 Err(_) => {
                     eprintln!("{:?} since network activity", duration)
+                    // request for missing indexes
                 }
             }
         }
     });
 
     // Listen for network activity
+
     let server = task::spawn({
         let debounce_tx = debounce_tx.clone();
         async move {
             while let Some(bytes) = network_rx.recv().await {
+                start = true;
                 // Received a packet
-                debounce_tx
-                    .send(bytes)
-                    .await
-                    .expect("Unable to talk to debounce");
-              //  eprintln!("Received a packet: {:?}", bytes.clone());
+                debounce_tx.send(bytes).await.expect("Unable to talk to debounce");
+                //  eprintln!("Received a packet: {:?}", bytes.clone());
             }
         }
     });
 
-  // Prevent deadlocks
+    // Prevent deadlocks
     drop(debounce_tx);
 
-
-
-/*
+    /*
     // Drive the network input
     network_tx.send(vec![1, 2, 3]).await.expect("Unable to talk to network");
     network_tx.send(vec![4, 2, 3]).await.expect("Unable to talk to network");
