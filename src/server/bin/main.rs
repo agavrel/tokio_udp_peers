0533b13ad44c61670efba3715ffb486a0775f163
agavrel <gavrel.antonin@gmail.com>
2021-03-31 14:21:09 -0400

    [ADD] Almost there

--- src/server/bin/main.rs
+++ src/server/bin/main.rs
@@ -1,9 +1,11 @@
-use std::time::Duration;
-use tokio::{sync::mpsc, task, time, net::{TcpStream}, io::{AsyncWriteExt}}; // 1.4.0
-use tokio::net::UdpSocket;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-
 use std::env;
+use std::net::SocketAddr;
+use std::sync::Arc;
+use std::time::Duration;
+use tokio::net::UdpSocket;
+use tokio::{sync::mpsc, task, time}; // 1.4.0
+use std::rc::Rc;
+
 const UDP_HEADER: usize = 8;
 const IP_HEADER: usize = 20;
 const AG_HEADER: usize = 4;
@@ -15,6 +17,8 @@ const ADDRESS_CLIENT: &str = "127.0.0.1:8000";
 
 #[tokio::main]
 async fn main() {
+    //server().await;
+
     match std::env::args().nth(1).as_deref() {
         Some("client") => client().await,
         _ => server().await,
@@ -23,127 +27,116 @@ async fn main() {
 
 async fn server() {
     eprintln!("Starting the server");
-
     let addr = env::args().nth(1).unwrap_or_else(|| ADDRESS.to_string());
-     eprintln!("{}", addr);
     let socket = UdpSocket::bind(&addr).await.unwrap();
+    let arc = Arc::new(socket);
+    let mut buf = [0u8; MAX_DATA_LENGTH];
+    let (debounce_tx, mut debounce_rx) = mpsc::channel::<Vec<u8>>(MAX_DATAGRAM_SIZE);
 
-    //let listener = TcpListener::bind(ADDRESS).await.expect("Unable to bind");
+    let _debouncer = task::spawn(async move {
+        let mut _packet_ids: Vec<i32> = Vec::new();
+        _packet_ids = vec![0; 10];
+        let duration = Duration::from_millis(1300);
 
+        loop {
+            match time::timeout(duration, debounce_rx.recv()).await {
+                Ok(Some(bytes)) => {
+                    let id: u8 = bytes.clone()[0];
+                    _packet_ids[id as usize] = 1;
+                    eprintln!("{} id packet received:{:?}", id, _packet_ids);
+                    if _packet_ids.iter().all(|x| x == &1i32) {
+                        println!("All packets have been received, stop program ");
+                        //   break;
+                    }
+                }
+                Ok(None) => {
+                    eprintln!("Done: {:?}", _packet_ids);
+                    break;
+                }
+                Err(_) => {
+                    eprintln!("No activity for 1.3sd");
+                }
+            }
+        }
+    });
+    // Listen for first packet
+    let result = arc.clone().recv_from(&mut buf).await;
+    match result {
+        Ok((len, addr)) => {
+            eprintln!("Bytes len: {} from {}", len, addr);
+            debounce_tx.send(buf.to_vec()).await.expect("Unable to talk to debounce");
+        }
+        Err(_) => {
+            eprintln!("Couldnt get datagram");
+        }
+    }
+    // listen for other packets
     loop {
-        let mut buf = [0u8; MAX_DATA_LENGTH];
-        let bytes= socket.recv_from(&mut buf).await.unwrap();
-        let (debounce_tx, mut debounce_rx) = mpsc::channel::<Vec<u8>>(1300); // mpsc::channel<Vec<u8>>(3300);
-            let (mut network_tx, mut network_rx) = mpsc::channel::<Vec<u8>>(1300);
-println!("gaaaaaa");
-        // Listen for events
-        let debouncer = task::spawn(async move {
-            let mut missing_ids: Vec<i32> = Vec::new();
-                missing_ids = vec![0; 10];
-            let duration = Duration::from_millis(1300);
+        let thread_socket = arc.clone();
+        let debounce_tx = debounce_tx.clone();
+    /*    let _server = task::spawn({
 
-            loop {
-            eprintln!("heeeeyeytrsrthyrth");
-                match time::timeout(duration, debounce_rx.recv()).await {
-                    Ok(Some(bytes)) => {
-                    eprintln!("{:?}", bytes);
-                        eprintln!("Network activity");
-                        let id:u8 = bytes.clone()[0];
-                        //eprintln!("id: {} {:?}", id, bytes);
-                        eprintln!("id: {}", id);
-                        missing_ids[id as usize] = 1;
-                        eprintln!("{:?}", missing_ids);
 
-                        if missing_ids.iter().all(|x| x == &1i32) {
-                            println!("FINISHHHHHHH ");
-                            break;
-                            //break;
+            async move {*/
+                while let result = thread_socket.recv_from(&mut buf).await {
+                    match result {
+                        Ok((len, addr)) => {
+                            eprintln!("Bytes len: {} from {}", len, addr);
+                            debounce_tx
+                                .send(buf.to_vec())
+                                .await
+                                .expect("Unable to talk to debounce");
+                        }
+                        Err(_) => {
+                            eprintln!("Couldnt get datagram");
                         }
                     }
-                    Ok(None) => {
-                        eprintln!("Done: {:?}", missing_ids);
-
-                        break;
-                    }
-                    Err(_) => {
-                        eprintln!("Error: {:?}", missing_ids);
-
-                    }
                 }
-            }
-        });
-
-        // Listen for network activity
-        let server = task::spawn({
-            let debounce_tx = debounce_tx.clone();
-            async move {
-                let mut buf = [0u8; MAX_DATA_LENGTH];
-                while let Some(bytes) = network_rx.recv().await {
-                    eprintln!("Bytes received: {:?}", bytes);
-                    // let (len, addr) = socket.recv_from(&mut buf).await.unwrap() { //
-
-                    debounce_tx
-                        .send(bytes)
-                        .await
-                        .expect("Unable to talk to debounce");
-                }
-            }
-        });
-
-        // Prevent deadlocks
-        drop(debounce_tx);
-
-
+                // Prevent deadlocks
+                drop(debounce_tx);
+         /*   }
+        });*/
     }
-
-
-/*
-
-   // Wait for everything to finish
-        server.await.expect("Server panicked");
-        debouncer.await.expect("Debouncer panicked");
-        */
-
-    //eprintln!("Server done");
+    // Wait for everything to finish
+    /* server.await.expect("Server panicked");
+       debouncer.await.expect("Debouncer panicked");
+    */
 }
 
 async fn client() {
     eprintln!("Starting the client");
 
-
-     let remote_addr: SocketAddr = env::args()
+    let remote_addr: SocketAddr = env::args()
         .nth(2)
-        .unwrap_or_else(|| "127.0.0.1:8080".into()) // cargo run --example udp-client -- 127.0.0.1:8080
-        .parse().unwrap();
+        .unwrap_or_else(|| ADDRESS.into()) // cargo run --example udp-client -- 127.0.0.1:8080
+        .parse()
+        .unwrap();
 
     // We use port 0 to let the operating system allocate an available port for us.
-
-
-
-    println!("Listening on: {}", ADDRESS_CLIENT);
+    let local_addr: SocketAddr = if remote_addr.is_ipv4() {
+        ADDRESS_CLIENT // "0.0.0.0:0" //
+    } else {
+        "[::]:0"
+    }
+    .parse()
+    .unwrap();
     let socket = UdpSocket::bind(ADDRESS_CLIENT).await.unwrap();
-    let data:[u8;5] = [1, 2, 3, 4, 5] ;
 
-    let len = socket.send_to(&data, remote_addr).await.unwrap();
+    socket.connect(&remote_addr).await.unwrap();
 
-
-
- /*
-    socket.send(vec![4, 2, 3]).await.expect("Unable to talk to network");
-    socket.send(vec![3, 2, 3]).await.expect("Unable to talk to network");
-
+    socket.send(&[0, 2, 3]).await.expect("Unable to talk to network");
+    socket.send(&[1, 2, 3]).await.expect("Unable to talk to network");
     time::sleep(Duration::from_millis(1200)).await;
-
-    socket.send(vec![2, 2, 3]).await.expect("Unable to talk to network");
-
-    socket.send(vec![5, 2, 3]).await.expect("Unable to talk to network");
-
-    let mut rng = rand::thread_rng();
-    let c = 1; // change for different probability
-    let n1: u8 = rng.gen_range(0..c);
-    socket.send(vec![n1, 2, 3]).await.expect("Unable to talk to network"); // stop when n1 = 0
-
+    socket.send(&[2, 2, 3]).await.expect("Unable to talk to network");
+    socket.send(&[3, 2, 3]).await.expect("Unable to talk to network");
+    socket.send(&[4, 2, 3]).await.expect("Unable to talk to network");
+    socket.send(&[5, 2, 3]).await.expect("Unable to talk to network");
+    socket.send(&[6, 2, 3]).await.expect("Unable to talk to network");
+    socket.send(&[7, 2, 3]).await.expect("Unable to talk to network");
     time::sleep(Duration::from_millis(1200)).await;
-*/
+    socket.send(&[8, 2, 3]).await.expect("Unable to talk to network");
+    time::sleep(Duration::from_millis(3200)).await;
+    socket.send(&[9, 2, 3]).await.expect("Unable to talk to network"); // stop when n1 = 0
+
     eprintln!("Client done");
 }
