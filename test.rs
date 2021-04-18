f2f15e110c799bcdf7b6f3a4167ae02b6b21651e
agavrel <gavrel.antonin@gmail.com>
2021-04-17 22:31:12 -0400

    [ADD] Crate and working example to encode and decode QR codes

--- test.rs
+++ test.rs
@@ -1,3 +1,50 @@
+use qrcode::QrCode; // QR encoding
+use image::Luma;
+use rqrr; // QR decoding
+
+
+fn decode_qr(s : &[u8]) -> Result<(), rqrr::DeQRError> {
+    let img = image::open("/tmp/qrcode.png").unwrap().to_luma8();
+    // Prepare for detection
+    let mut img = rqrr::PreparedImage::prepare(img);
+    // Search for grids, without decoding
+    let grids = img.detect_grids();
+    assert_eq!(grids.len(), 1);
+    // Decode the grid
+    let (meta, content) = grids[0].decode()?;
+    assert_eq!(meta.ecc_level, 0);
+    assert_eq!(content.as_bytes(), s);
+
+    Ok(())
+}
+
+fn main()  {
+    // Encode some data into bits.
+    let s:&[u8] = b"https://www.google.com";
+    let code = QrCode::new(s).unwrap();
+
+    // Render the bits into an image.
+    let image = code.render::<Luma<u8>>().build();
+
+    // Save the image.
+    image.save("/tmp/qrcode.png").unwrap();
+
+    // You can also render it into a string.
+    let string = code.render()
+        .light_color(' ')
+        .dark_color('#')
+        .build();
+    println!("{}", string);
+
+    let result = decode_qr(s);
+    match result {
+         Err(e) => println!("error parsing QR code: {:?}", e),
+         Ok(v) => println!("working QR code"),
+        }
+
+}
+
+/*
 
 use sodiumoxide::randombytes::randombytes;
 
@@ -8,6 +55,8 @@ fn main() {
     chunks_list = v.chunks(n).collect();
     println!("{:?}", chunks_list);
 }
+
+*/
 
 //   chunks_list = chunks.into_iter().map(|x| x).collect();
 
